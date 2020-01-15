6653f72d310070b8dc60ea307d8a8bbffb674005
kersevan <gregor.kersevan@gmail.com>
2020-01-15 20:33:43 +0100

    added ratings for valid productIds

--- services/src/main/java/si/rso/ratings/services/impl/AdminServiceImpl.java
+++ services/src/main/java/si/rso/ratings/services/impl/AdminServiceImpl.java
@@ -21,7 +21,7 @@ public class AdminServiceImpl implements AdminService {
     
     @Override
     public List<Rating> populateData() {
-        String productId = "caa08db8-b2c3-43e8-b419-542126b841bd";
+        String productId = "d519b320-80a3-40e2-ad96-7edd8c878630";
     
         Document document = new Document();
         document.put(Rating.FIELD_PRODUCT_ID, productId);
@@ -33,6 +33,39 @@ public class AdminServiceImpl implements AdminService {
         document.put(Rating.FIELD_PRODUCT_ID, productId);
         document.put(Rating.FIELD_RATING_NUM, 2);
         document.put(Rating.FIELD_COMMENT, "just bad");
+        mongoService.insertDocument(document);
+
+        productId = "fbace5c1-653c-42c0-aa02-78cc4ea4fac1";
+        document = new Document();
+        document.put(Rating.FIELD_PRODUCT_ID, productId);
+        document.put(Rating.FIELD_RATING_NUM, 5);
+        document.put(Rating.FIELD_COMMENT, "Excellent");
+        mongoService.insertDocument(document);
+
+        document = new Document();
+        document.put(Rating.FIELD_PRODUCT_ID, productId);
+        document.put(Rating.FIELD_RATING_NUM, 4);
+        document.put(Rating.FIELD_COMMENT, "Great");
+        mongoService.insertDocument(document);
+
+        productId = "66100dac-ff08-4ac2-9c1a-fa5120ff4838";
+        document = new Document();
+        document.put(Rating.FIELD_PRODUCT_ID, productId);
+        document.put(Rating.FIELD_RATING_NUM, 3);
+        document.put(Rating.FIELD_COMMENT, "Average");
+        mongoService.insertDocument(document);
+
+        document = new Document();
+        document.put(Rating.FIELD_PRODUCT_ID, productId);
+        document.put(Rating.FIELD_RATING_NUM, 2);
+        document.put(Rating.FIELD_COMMENT, "Poor packaging");
+        mongoService.insertDocument(document);
+
+        productId = "18c18291-6541-4317-a374-b9a03b65b90a";
+        document = new Document();
+        document.put(Rating.FIELD_PRODUCT_ID, productId);
+        document.put(Rating.FIELD_RATING_NUM, 3);
+        document.put(Rating.FIELD_COMMENT, "Average");
         mongoService.insertDocument(document);
     
         return ratingService.getProductRatings(productId);
